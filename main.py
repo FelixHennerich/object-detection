25a24b997f255a4fd0f2f7342b56a7a7f28f7a26
Felix Hennerich <66116721+BlueNitrox@users.noreply.github.com>
2023-11-19 14:28:09 +0100

    detection laggy working

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import cv2
 from ultralytics import YOLO
+import cv2
+import math 
+# start webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+# model
 model = YOLO("yolo-Weights/yolov8n.pt")
 
+# object classes
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
               "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
@@ -15,16 +23,40 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               ]
 
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-
 while True:
-    ret, img= cap.read()
+    success, img = cap.read()
     results = model(img, stream=True)
 
-    cv2.imshow('Webcam', frame)
+    # coordinates
+    for r in results:
+        boxes = r.boxes
 
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
+
+            # class name
+            cls = int(box.cls[0])
+            print("Class name -->", classNames[cls])
+
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+    cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
         break
 
