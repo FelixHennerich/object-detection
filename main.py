aadb6abd945381c53f9b4a2c0c3e471c866981c7
Felix Hennerich <66116721+BlueNitrox@users.noreply.github.com>
2023-11-19 14:25:53 +0100

    detection try

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import cv2
+from ultralytics import YOLO
+model = YOLO("yolo-Weights/yolov8n.pt")
 
-frameWidth = 640
-frameHeight = 480
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+
 cap = cv2.VideoCapture(0)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
-cap.set(10,150)
+cap.set(3, 640)
+cap.set(4, 480)
 
-while cap.isOpened():
-    success, img = cap.read()
-    if success:
-        cv2.imshow("Result", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+while True:
+    ret, img= cap.read()
+    results = model(img, stream=True)
+
+    cv2.imshow('Webcam', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
